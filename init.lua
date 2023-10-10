8a657ee18b0f725387d72c4dbd4f4c91953b5be4
Aldric.li <2746763180@qq.com>
2023-10-10 18:04:56 +0800

    全局配置隔离修改

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
---包导入
+--全局设定
 require('keymap')
 require('plugins')
+require('option')
 
 -- 全局设置
-vim.g.mapleader = ' '
 
 
 -- 全局插件
@@ -12,42 +12,45 @@ require('leap').add_default_mappings()
 
 if vim.g.vscode then
     --仅用于vscode的插件
-    vim.opt.timeoutlen = 500
-    vim.opt.clipboard = 'unnamedplus' -- use system clipboar
 else
     -- 仅用于neovim的插件
-    require('option')
     require('lsp')
+
     require('colorscheme')
+
     require('nvim-autopairs').setup()
+
     require('lualine').setup()
-    require('nvim-tree').setup({ -- 关闭文件时自动关闭
-        -- auto_close = true,
+
+    require('nvim-tree').setup { -- 关闭文件时自动关闭
         -- 不显示 git 状态图标
         git = {
             enable = false
         },
-        require 'nvim-treesitter.configs'.setup {
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
+    }
+
+    require 'nvim-treesitter.configs'.setup {
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
         },
-        require('toggleterm').setup {
-            size = 10,
-            open_mapping = [[<A-q>]],
-            direction = 'float',
-            cmd = 'powershell -NoLogo',
-            start_in_insert = true,
-            shell = 'pwsh.exe',
-            float_opts = {
-                autochdir = true,
-                border = 'curved',
-                width = math.ceil(vim.o.columns * 0.8),
-                height = math.ceil(vim.o.columns * 0.2)
-            }
+    }
+
+    require('toggleterm').setup {
+        size = 10,
+        open_mapping = [[<A-q>]],
+        direction = 'float',
+        cmd = 'powershell -NoLogo',
+        start_in_insert = true,
+        shell = 'pwsh.exe',
+        float_opts = {
+            autochdir = true,
+            border = 'curved',
+            width = math.ceil(vim.o.columns * 0.8),
+            height = math.ceil(vim.o.columns * 0.2)
         }
-    })
+    }
+
     require('tabout').setup {
         tabkey = '<Tab>',             -- key to trigger tabout, set to an empty string to disable
         backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
