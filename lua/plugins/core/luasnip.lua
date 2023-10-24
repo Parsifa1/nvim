472e9397c88ad86c2f979daecf3680b3b25cd340
Parsifa1 <2746763180@qq.com>
2023-10-24 17:21:58 +0800

    增加snippets文件夹，用于存放snippets

---
+++ lua/plugins/core/luasnip.lua
@@ -0,0 +1,19 @@
+return {
+    'L3MON4D3/LuaSnip',
+    build = "make install_jsregexp",
+    event = {
+        "InsertEnter",
+        "CmdlineEnter",
+    },
+    config = function ()
+        local snip = require "luasnip"
+        -- local i = snip.insert_node
+        -- local sn = snip.snippet_node
+        require("luasnip").config.setup({
+            enable_autosnippets = true
+        })
+        require("luasnip.loaders.from_lua").lazy_load {
+            paths = vim.fn.stdpath "config" .. "/lua/snippets",
+        }
+    end
+}
