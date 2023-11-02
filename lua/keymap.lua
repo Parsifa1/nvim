a8e520b3f671a1ab065fe8417a76e386c8a55e69
Parsifa1 <2746763180@qq.com>
2023-11-02 17:22:16 +0800

    添加rust自动运行

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -81,13 +81,22 @@ else
                 '<Esc>:FloatermNew --autoclose=0 cd %:h; g++ %:t -std=c++20 -o bin/%:t:r; time ./bin/%:t:r<CR>', opt)
         end
     })
-    -- --python
+    --python
     vim.api.nvim_create_autocmd('filetype', {
         pattern = "python",
         callback = function()
             local opt = { noremap = true, silent = true, buffer = true }
-            vim.keymap.set('n', '<F5>', ':FloatermNew --autoclose=0 time python % <CR>', opt)
-            vim.keymap.set('i', '<F5>', '<Esc>:FloatermNew --autoclose=0 time python % <CR>', opt)
+            vim.keymap.set('n', '<f5>', ':FloatermNew --autoclose=0 time python % <cr>', opt)
+            vim.keymap.set('i', '<f5>', '<esc>:FloatermNew --autoclose=0 time python % <cr>', opt)
+        end
+    })
+    --rust
+    vim.api.nvim_create_autocmd('filetype', {
+        pattern = "rust",
+        callback = function()
+            local opt = { noremap = true, silent = true, buffer = true }
+            vim.keymap.set('n', '<f5>', ':FloatermNew --autoclose=0 cd %:h; time cargo run<CR>', opt)
+            vim.keymap.set('i', '<f5>', '<esc>:FloatermNew --autoclose=0 cd %:h; time cargo run<CR>', opt)
         end
     })
 
