506462df33b7d411d104dd26aac24676ce8bd288
Parsifa1 <2746763180@qq.com>
2023-12-01 03:22:22 +0800

    move treesitter to core && move mason-installer to mason

--- lua/plugins/core/Mason.lua
+++ lua/plugins/core/Mason.lua
@@ -1,18 +1,40 @@
 return {
-    "williamboman/mason.nvim",
-    event = {
-        "InsertEnter",
-        "CmdlineEnter",
-    },
-    config = function()
-        require("mason").setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
-        })
-    end
+	"williamboman/mason.nvim",
+	dependencies = {
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	event = {
+		"InsertEnter",
+		"CmdlineEnter",
+	},
+	config = function()
+		require("mason").setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"lua-language-server",
+				"clangd",
+				"codelldb",
+				"rust-analyzer",
+				"python-lsp-server",
+				"pyright",
+				"clang-format",
+				"autopep8",
+				"marksman",
+				"yaml-language-server",
+				"haskell-language-server",
+				-- "cpptools",
+				-- "stylua",
+			},
+			-- auto_update = true,
+			-- run_on_start = true,
+		})
+	end,
 }
