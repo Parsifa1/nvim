cb38b259a498f2d379f9bbe9b0b9b2101c3b320d
Parsifa1 <2746763180@qq.com>
2023-12-01 22:51:08 +0800

    turn nvim-autopairs to ultimate-autopair

---
+++ lua/plugins/tools/ultimate-autopair.lua
@@ -0,0 +1,13 @@
+return {
+    "altermo/ultimate-autopair.nvim",
+    event = { "InsertEnter", "CmdlineEnter" },
+    opts = {
+        tabout = {
+            enable = true,
+            map = "<Tab>",
+            cmap = "<Tab>",
+            hopout = true,
+            do_nothing_if_fail = false,
+        },
+    },
+}
