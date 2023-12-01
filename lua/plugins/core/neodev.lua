49a4c6a04e33c5fbd9119fbc74b756241fd0519e
Parsifa1 <2746763180@qq.com>
2023-12-02 00:12:04 +0800

    setting neodev

--- lua/plugins/core/neodev.lua
+++ lua/plugins/core/neodev.lua
@@ -1,13 +1,6 @@
 return {
     "folke/neodev.nvim",
     event = "VeryLazy",
-    opts = {
-        override = function(root_and_library)
-            local library = root_and_library.roo_dir
-            library.enabled = true
-            library.plugins = true
-        end,
-
-    }
+    opts = {}
 }
 
--- lua/plugins/core/nvim-lsp.lua
+++ lua/plugins/core/nvim-lsp.lua
@@ -6,32 +6,25 @@ local function firstToUpper(str)
     return (str:gsub("^%l", string.upper))
 end
 
-return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "lukas-reineke/lsp-format.nvim",
-        "ray-x/lsp_signature.nvim",
-    },
-    event = "User FileOpened",
-    config = function()
-        local lspconfig = require("lspconfig")
-        -- Set diagnostic icons
-        vim.iter(require("custom").icons.diagnostic):each(function(type, icon)
-            local hl = "DiagnosticSign" .. firstToUpper(type)
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end)
+local config = function()
+    local lspconfig = require("lspconfig")
+    -- Set diagnostic icons
+    vim.iter(require("custom").icons.diagnostic):each(function(type, icon)
+        local hl = "DiagnosticSign" .. firstToUpper(type)
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end)
 
-        -- Customized on_attach function
-        vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float, desc("open float"))
-        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, desc("goto prev"))
-        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, desc("goto next"))
-        vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, desc("quickfix list"))
+    -- Customized on_attach function
+    vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float, desc("open float"))
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, desc("goto prev"))
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, desc("goto next"))
+    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, desc("quickfix list"))
 
-        -- Use an on_attach function to only map the following keys
-        local on_attach = function(ev)
-            local bufnr = ev.bufnr
+    -- use autocmd to automatically sign up lsp
+    vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "General LSP Attach",
+        callback = function(args)
+            local bufnr = args.bufnr
             local function bufopts_desc(index)
                 return { noremap = true, silent = true, buffer = bufnr, desc = index }
             end
@@ -56,64 +49,82 @@ return {
                 signs = true,
                 severity_sort = true,
             })
-        end
+        end,
+    })
 
-        --for lsp signature
-        require("lsp_signature").setup({
-            hint_prefix = "🧐 ", --  NOTE: for the terminal not support emoji, might crash
-            handler_opts = {
-                border = require("custom").border,
-            },
-        })
+    --for lsp signature
+    require("lsp_signature").setup({
+        hint_prefix = "🧐 ", --  NOTE: for the terminal not support emoji, might crash
+        handler_opts = {
+            border = require("custom").border,
+        },
+    })
 
-        -- for inlay hints
-        vim.api.nvim_create_autocmd("LspAttach", {
-            desc = "General LSP Attach",
-            callback = function(args)
-                local bufnr = args.buf
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if client and client.server_capabilities.inlayHintProvider then
-                    vim.lsp.inlay_hint.enable(bufnr, true)
-                end
-            end,
-        })
+    -- for inlay hints
+    vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "General LSP Attach",
+        callback = function(args)
+            local bufnr = args.buf
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client and client.server_capabilities.inlayHintProvider then
+                vim.lsp.inlay_hint.enable(bufnr, true)
+            end
+        end,
+    })
 
-        -- for lsp config in mason
-        require("mason-lspconfig").setup_handlers {
-            function(server_name) -- default handler (optional)
-                require("lspconfig")[server_name].setup {
-                    on_attach = on_attach,
-                }
-            end,
-            ["clangd"] = function()
-                lspconfig.clangd.setup({
-                    on_attach = on_attach,
-                    filetypes = { "cpp", "c" },
-                    cmd = {
-                        "clangd",
-                        "--offset-encoding=utf-16",
-                    },
-                })
-            end,
-            ["lua_ls"] = function()
-                lspconfig.lua_ls.setup({
-                    on_attach = on_attach,
-                    settings = {
-                        Lua = {
-                            hint = {
-                                enable = true,
-                                arrIndex = "Enable",
-                                setType = true,
-                            },
-                            diagnostics = {
-                                disable = { "missing-fields", "incomplete-signature-doc" },
-                                globals = { "vim" },
-                            },
+    -- for neodev
+    require("neodev").setup({
+        override = function(root_and_library)
+            local library = root_and_library.roo_dir
+            library.enabled = true
+            library.plugins = true
+        end,
+    })
+
+    -- for lsp config in mason
+    require("mason-lspconfig").setup_handlers {
+        function(server_name)     -- default handler (optional)
+            require("lspconfig")[server_name].setup {}
+        end,
+        ["clangd"] = function()
+            lspconfig.clangd.setup({
+                filetypes = { "cpp", "c" },
+                cmd = {
+                    "clangd",
+                    "--offset-encoding=utf-16",
+                },
+            })
+        end,
+        ["lua_ls"] = function()
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        hint = {
+                            enable = true,
+                            arrIndex = "Enable",
+                            setType = true,
+                        },
+                        diagnostics = {
+                            disable = { "missing-fields", "incomplete-signature-doc" },
+                            globals = { "vim" },
                         },
                     },
-                })
-            end
-        }
-    end,
+                },
+            })
+        end
+    }
+end
+
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "lukas-reineke/lsp-format.nvim",
+        "ray-x/lsp_signature.nvim",
+        "folke/neodev.nvim",
+    },
+    event = "User FileOpened",
+    config = config
 }
 
