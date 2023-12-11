e125f6f75eacde8c6a84a0f0a47e1eab4279802f
Parsifa1 <2746763180@qq.com>
2023-12-12 02:16:23 +0800

    add some keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -10,11 +10,20 @@ else
     set("v", "=", "gq")
     set("n", "=", "gq")
     set("o", "=", "q")
+
     --窗口操作
     set("n", "<up>", ":res +5<CR>")
     set("n", "<down>", ":res -5<CR>")
     set("n", "<left>", ":vertical resize-5<CR>")
     set("n", "<right>", ":vertical resize+5<CR>")
+
+    --搜索保持光标
+    set("n", "n", "nzzzv")
+    set("n", "N", "Nzzzv")
+
+    -- 行移动
+    set("v", "J", ":m '>+1<CR>gv=gv")
+    set("v", "K", ":m '<-2<CR>gv=gv")
 
     -- 展开/关闭终端
     set("n", "`", ":ToggleTerm<CR>", "Terminal")
@@ -43,6 +52,9 @@ else
     set("v", "<C-c>", "y")
     set("i", "<C-v>", "<Esc>pa")
     set("n", "<C-v>", "p")
+    set("n", "<C-d>", "<C-d>zz")
+    set("n", "<C-u>", "<C-u>zz")
+
     --CP相关
     set("n", "<F1>", ":CompetiTest run<CR>")
     set("i", "<F1>", "<Esc>:CompetiTest run<CR>")
@@ -50,6 +62,7 @@ else
     set("i", "<F2>", "<Esc>:CompetiTest add_testcase<CR>")
     set("n", "<F3>", ":CompetiTest edit_testcase<CR>")
     set("i", "<F3>", "<Esc>:CompetiTest edit_testcase<CR>")
+
     --task指令
     set("n", "<F4>", ":AsyncTask acm<CR>", "run acm")
     set("n", "<F5>", ":AsyncTask file-run<CR>", "run task")
@@ -62,6 +75,7 @@ else
     set("n", "<leader>l", "<C-w>l", "goto right window")
     set("n", "<leader>a", ":AerialToggle<CR>", "Outline")
     set("n", "<leader>e", ":lua MiniFiles.open()<CR>", "File Explorer")
+    set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<left><left><left>")
     set("n", "<leader>p", ":Lazy<CR>", "lazy")
     set("n", "<leader>v", ":lua vim.fn.chdir(vim.fn.fnamemodify(vim.fn.expand('%:p'), ':h'))<CR>", "cd current file")
     set("n", "<leader>i", ":Telescope neovim-project history theme=dropdown<CR><Esc>", "projects folder")
@@ -71,5 +85,6 @@ else
     set("n", "<leader>cc", ":Telescope commands<CR>", "commands")
     set("n", "<leader>ck", ":Telescope keymaps<CR>", "keymaps")
     set("n", "<leader>gg", ":Neogit<CR>", "Open Neogit")
+    set("n", "<leader>sw", ":set wrap!<CR>", "toggle warp")
 end
 
