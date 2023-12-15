c2832a06e22cf41c23642b9b55b711e0fa891f9b
Parsifa1 <li.aldric@gmail.com>
2023-12-16 01:34:07 +0800

    fix <C-v> in insert mode

--- lua/option.lua
+++ lua/option.lua
@@ -66,7 +66,7 @@ else
         vim.keymap.set('n', '<C-v>', 'P')       -- Paste normal mode
         vim.keymap.set('v', '<C-v>', 'P')       -- Paste visual mode
         vim.keymap.set('c', '<C-v>', '<C-R>+')   -- Paste command mode
-        vim.keymap.set('i', '<C-v>', '<ESC>lpli') -- Paste insert mode
+        vim.keymap.set('i', '<C-v>', '<ESC>pa') -- Paste insert mode
     end
 end
 
