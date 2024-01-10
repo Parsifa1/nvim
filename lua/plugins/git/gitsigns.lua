4b6387b654e2074fae5603b63dfb504e4897be5e
Parsifa1 <li.aldric@gmail.com>
2024-01-10 10:35:04 +0800

    update: change git signs

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -8,9 +8,9 @@ return {
 
         gitsigns.setup {
             signs = {
-                add          = { text = '+' },
-                change       = { text = '+' },
-                delete       = { text = '-' },
+                add          = { text = ' ' },
+                change       = { text = ' ' },
+                delete       = { text = ' ' },
                 topdelete    = { text = '-' },
                 changedelete = { text = '~' },
                 untracked    = { text = '' },
