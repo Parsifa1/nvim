78429ce5a5c76865859d33a6aafa089d81e5b743
Parsifa1 <li.aldric@gmail.com>
2024-01-10 10:47:05 +0800

    fix:change copilot key to <Ctrl-Enter>

--- lua/plugins/tools/copilot.lua
+++ lua/plugins/tools/copilot.lua
@@ -1,5 +1,5 @@
 return {
     'github/copilot.vim',
     event = "VeryLazy",
-    vim.api.nvim_set_keymap("i", "J", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.api.nvim_set_keymap("i", "<NL>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 }
