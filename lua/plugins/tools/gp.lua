4b7840e88ee7a85544e2b5370305a4b356f40070
Parsifa1 <li.aldric@gmail.com>
2024-02-01 07:15:54 +0800

    update: close gp

--- lua/plugins/tools/gp.lua
+++ lua/plugins/tools/gp.lua
@@ -1,6 +1,7 @@
 return {
     "robitx/gp.nvim",
     event = "VeryLazy",
+    enabled = false,
     config = function()
         require("gp").setup {
             openai_api_key = { "cat", "/home/parsifa1/Public/openai_api_key" },
