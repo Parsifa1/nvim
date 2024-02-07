5ff043c6140bd81789e014c40ea787a02f37b386
Parsifa1 <li.aldric@gmail.com>
2024-02-07 20:34:57 +0800

    fix: workspace ans session_manager

--- lua/plugins/tools/workspace.lua
+++ lua/plugins/tools/workspace.lua
@@ -3,7 +3,9 @@ return {
     dependencies = { "Shatur/neovim-session-manager" },
     config = function()
         require("telescope").load_extension "workspaces"
-        require("session_manager").setup { autoload_mode = "Disabled" }
+        require("session_manager").setup {
+            autoload_mode = "Disabled",
+        }
         require("workspaces").setup {
             auto_open = true,
             sort = true,
@@ -11,8 +13,11 @@ return {
             hooks = {
                 add = { "SessionManager save_current_session" },
                 open = { "SessionManager load_current_dir_session" },
-                remove = { "SessionManager delete_current_dir_session"}
+                remove = { "SessionManager delete_current_dir_session" },
             },
         }
+        vim.api.nvim_create_autocmd({ "DirChangedPre" }, {
+            callback = require("session_manager").autosave_session,
+        })
     end,
 }
