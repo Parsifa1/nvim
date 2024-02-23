0cc82807d50109f2cb16bc20526977655b97f1ac
Parsifa1 <li.aldric@gmail.com>
2024-02-23 17:13:04 +0800

    fix:add fish for nvim-colorizer

--- lua/plugins/ui/nvim-colorizer.lua
+++ lua/plugins/ui/nvim-colorizer.lua
@@ -8,6 +8,7 @@ return {
         "javascriptreact",
         "typescriptreact",
         "lua",
+        "fish",
     },
     config = function()
         require("colorizer").setup {}
--- lua/plugins/ui/rainbow-delimiters.lua
+++ lua/plugins/ui/rainbow-delimiters.lua
@@ -2,10 +2,8 @@ return {
     "hiphish/rainbow-delimiters.nvim",
     event = "BufReadPre",
     config = function()
-        -- This module contains a number of default definitions
         local rainbow_delimiters = require "rainbow-delimiters"
 
-        ---rainbow_delimiters.config
         vim.g.rainbow_delimiters = {
             strategy = {
                 [""] = rainbow_delimiters.strategy["global"],
