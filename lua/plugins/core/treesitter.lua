ba9eb644c8888a4d5378d0c0e7b8e73b96926080
Parsifa1 <li.aldric@gmail.com>
2024-02-29 23:37:32 +0800

    add:nvim-treesitter-context

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -1,34 +1,46 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = "BufReadPre",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup {
-            ensure_installed = {
-                "cpp",
-                "lua",
-                "rust",
-                "python",
-                "markdown",
-                "vim",
-                "regex",
-                "bash",
-                "markdown_inline",
-                "comment",
-                "org",
-                "yaml",
-                "fish",
-                "html",
-                "javascript",
-                "typescript",
-                "latex",
-            },
-            sync_install = true,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        }
-    end,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = "BufReadPre",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = {
+                    "cpp",
+                    "lua",
+                    "rust",
+                    "python",
+                    "markdown",
+                    "vim",
+                    "regex",
+                    "bash",
+                    "markdown_inline",
+                    "comment",
+                    "org",
+                    "yaml",
+                    "fish",
+                    "html",
+                    "javascript",
+                    "typescript",
+                    "latex",
+                },
+                sync_install = true,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            }
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        -- enabled = false,
+        event = "BufReadPre",
+        config = function()
+            require("treesitter-context").setup {
+                mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
+            }
+        end,
+    },
 }
