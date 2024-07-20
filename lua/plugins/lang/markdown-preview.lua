5a6b196e5439fc487b07d20129f1afcff12b6d0b
Parsifa1 <li.aldric@gmail.com>
2024-07-20 13:50:26 +0000

    fix: mkdp install

--- lua/plugins/lang/markdown-preview.lua
+++ lua/plugins/lang/markdown-preview.lua
@@ -3,8 +3,6 @@
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function()
-        vim.fn["mkdp#util#install"]()
-    end,
+    build = ":cal mkdp#util#install()",
     ft = { "markdown" },
 }
