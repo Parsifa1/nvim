14484b5899f85543c764f5ce8e98d76b89579df4
Parsifa1 <li.aldric@gmail.com>
2024-11-18 03:34:26 +0800

    feat: open image.nvim

--- lua/plugins/tools/image.lua
+++ lua/plugins/tools/image.lua
@@ -1,10 +1,15 @@
 return {
     "3rd/image.nvim",
-    enabled = false,
+    -- enabled = false,
     ft = "markdown",
     config = function()
         require("image").setup {
             backend = "kitty",
+            integrations = {
+                markdown = {
+                    only_render_image_at_cursor = true
+                }
+            }
         }
     end,
 }
