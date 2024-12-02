29695773668f5985f04e50813d09ba02d6b5d125
Parsifa1 <li.aldric@gmail.com>
2024-12-02 16:36:14 +0000

    feat: add blink auto bracket

--- lua/plugins/lsp/blink.lua
+++ lua/plugins/lsp/blink.lua
@@ -21,11 +21,13 @@ return {
                     },
                 },
             },
-            documentation = {
-                auto_show = true,
-                window = {
-                    border = "rounded",
-                    scrollbar = false,
+            accept = {
+                auto_brackets = {
+                    enabled = true,
+                    kind_resolution = {
+                        enabled = true,
+                        blocked_filetypes = { "typescriptreact", "javascriptreact", "vue", "rust" },
+                    },
                 },
             },
         },
@@ -34,9 +36,18 @@ return {
             window = { border = "rounded" },
         },
         snippets = {
-            expand = nil,
-            active = nil,
-            jump = nil,
+            expand = function(snippet)
+                require("luasnip").lsp_expand(snippet)
+            end,
+            active = function(filter)
+                if filter and filter.direction then
+                    return require("luasnip").jumpable(filter.direction)
+                end
+                return require("luasnip").in_snippet()
+            end,
+            jump = function(direction)
+                require("luasnip").jump(direction)
+            end,
         },
         sources = {
             completion = {
