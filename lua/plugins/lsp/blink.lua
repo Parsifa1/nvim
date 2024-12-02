3182cdc36ebe170332781dde0f3bd0902fe233fe
Parsifa1 <li.aldric@gmail.com>
2024-12-02 16:12:11 +0000

    fix: snippet support

--- lua/plugins/lsp/blink.lua
+++ lua/plugins/lsp/blink.lua
@@ -34,18 +34,9 @@ return {
             window = { border = "rounded" },
         },
         snippets = {
-            expand = function(snippet)
-                require("luasnip").lsp_expand(snippet)
-            end,
-            active = function(filter)
-                if filter and filter.direction then
-                    return require("luasnip").jumpable(filter.direction)
-                end
-                return require("luasnip").in_snippet()
-            end,
-            jump = function(direction)
-                require("luasnip").jump(direction)
-            end,
+            expand = nil,
+            active = nil,
+            jump = nil,
         },
         sources = {
             completion = {
