796a95fc2f4e98a7f3ea81d545cf329c685b131a
Parsifa1 <li.aldric@gmail.com>
2024-12-11 16:45:20 +0000

    fix: package-info

--- lua/plugins/lang/package-info.lua
+++ lua/plugins/lang/package-info.lua
@@ -1,9 +1,22 @@
 return {
     "vuki656/package-info.nvim",
-    ft = "json",
-    commit = "268f466",
     event = { "BufRead package.json" },
-    opts = {
-        package_manager = "pnpm",
-    },
+    ft = "json",
+    config = function()
+        require("package-info").setup {
+            autostart = false,
+            package_manager = "pnpm",
+        }
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "json",
+            callback = function()
+                -- 等待0.5秒
+                if vim.fn.expand "%:t" == "package.json" then
+                    vim.defer_fn(function()
+                        require("package-info").show()
+                    end, 500)
+                end
+            end,
+        })
+    end,
 }
