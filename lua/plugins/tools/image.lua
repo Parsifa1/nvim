db7a65a8d992c6b2d486121b345efc4828de9211
Parsifa1 <li.aldric@gmail.com>
2024-12-19 20:13:13 +0800

    feat: image typst support

--- lua/plugins/tools/image.lua
+++ lua/plugins/tools/image.lua
@@ -2,12 +2,15 @@ return {
     "3rd/image.nvim",
     enabled = vim.fn.hostname() ~= "debian",
     -- enabled = false,
-    ft = "markdown",
+    ft = { "markdown", "typst" },
     config = function()
         require("image").setup {
             backend = "kitty",
             integrations = {
                 markdown = {
+                    only_render_image_at_cursor = true
+                },
+                typst = {
                     only_render_image_at_cursor = true
                 }
             }
