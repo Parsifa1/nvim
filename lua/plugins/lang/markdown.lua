cb8e2b29558eb7bc3618b6fc7feeacbfdbef090e
Parsifa1 <li.aldric@gmail.com>
2025-06-08 15:50:37 +0800

    fix(markdown): fix rd for latex-math

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -29,11 +29,15 @@ return {
             local not_math = utils.with_opts(utils.not_math, true) -- true to use treesitter
 
             -- set a higher priority (defaults to 0 for most snippets)
-            local snip = ls.snippet(
+            local dm = ls.snippet(
                 { trig = "dm", name = "Math", condition = utils.pipe { not_math }, priority = 10 },
                 { t { "$$", "" }, i(0), t { " ", "" }, t { "$$", "" } }
             )
-            ls.add_snippets("markdown", { snip }, { type = "autosnippets" })
+            local rd = ls.parser.parse_snippet(
+                { trig = "rd", name = "to the ... power ^{}", utils.pipe { not_math }, priority = 10 },
+                "^{$1}$0 "
+            )
+            ls.add_snippets("markdown", { dm, rd }, { type = "autosnippets" })
         end,
     },
 }
