7edbf2bab06395d236b62b14b463099478bdac6c
Parsifa1 <li.aldric@gmail.com>
2025-06-16 20:59:42 +0800

    fix(mason): uing append PATH

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -13,6 +13,7 @@ return {
         },
         config = function()
             require("mason").setup {
+                PATH = "append", -- NOTE:为了兼容NIXOS
                 registries = { "github:mason-org/mason-registry" },
                 ui = {
                     backdrop = 100,
