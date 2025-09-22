b4d0d7f81c97526f9b45ae683ba6c423aa54fa9d
Parsifa1 <li.aldric@gmail.com>
2025-09-23 02:16:20 +0800

    feat(kulala): add kulala for rest testing

---
+++ lua/plugins/tools/kulala.lua
@@ -0,0 +1,12 @@
+return {
+  "mistweaverco/kulala.nvim",
+  keys = {
+    { "<leader>Rs", desc = "Send request" },
+    { "<leader>Ra", desc = "Send all requests" },
+    { "<leader>Rb", desc = "Open scratchpad" },
+  },
+  ft = { "http", "rest" },
+  opts = {
+    global_keymaps = true,
+  },
+}
