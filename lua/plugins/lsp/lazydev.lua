a94f49f49af6f4064b5dbbb69489383e9c058488
Parsifa1 <li.aldric@gmail.com>
2026-02-23 23:52:33 +0800

    fix(lua_ls): fix workspace autoload

--- lua/plugins/lsp/lazydev.lua
+++ lua/plugins/lsp/lazydev.lua
@@ -1,13 +1,10 @@
 ---@type LazyPluginSpec
 return {
   "folke/lazydev.nvim",
-  dependencies = { "Bilal2453/luvit-meta" },
   ft = "lua",
-  event = "LspAttach",
   opts = {
     library = {
-      "../../utils",
-      { path = "luvit-meta/library", words = { "vim%.uv", "vim%.loop" } },
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       { path = "lazy.nvim", words = { "LazyPluginSpec" } },
       { path = vim.env.VIMRUNTIME },
     },
--- lua/utils/lsp/lua_ls.lua
+++ lua/utils/lsp/lua_ls.lua
@@ -7,8 +7,18 @@ return {
         arrIndex = "Enable",
         setType = true,
       },
+      workspace = {
+        library = { vim.env.VIMRUNTIME },
+      },
       diagnostics = {
-        disable = { "missing-fields", "incomplete-signature-doc" },
+        disable = {
+          "missing-fields",
+          "incomplete-signature-doc",
+          "param-type-mismatch",
+          "assign-type-mismatch",
+          "undefined-doc-name",
+        },
+        globals = { "vim", "Snacks", "MiniFiles" },
       },
     },
   },
