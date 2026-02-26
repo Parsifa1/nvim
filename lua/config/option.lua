a260f2408da8bcc01d84633d558bc9e34f0a678a
Parsifa1 <li.aldric@gmail.com>
2026-02-26 20:38:51 +0800

    fix(clipboard): handle with which-key

--- lua/config/option.lua
+++ lua/config/option.lua
@@ -80,7 +80,17 @@ vim.g["loaded_node_provider"] = 0
 vim.g["loaded_perl_provider"] = 0
 vim.g["loaded_ruby_provider"] = 0
 
-vim.g.clipboard = "osc52"
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+    ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste "+",
+    ["*"] = require("vim.ui.clipboard.osc52").paste "*",
+  },
+}
 if vim.g.neovide then
   vim.g.clipboard = false
   vim.opt.fillchars:append { diff = "╱" }
