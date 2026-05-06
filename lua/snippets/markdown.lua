6448ea6dcfbeea4f329c827c4dac1110f72a0c8b
Parsifa1 <li.aldric@gmail.com>
2026-05-06 22:13:23 +0800

    feat(snippet): add `now` in markdown

--- lua/snippets/markdown.lua
+++ lua/snippets/markdown.lua
@@ -1,6 +1,7 @@
 local s = require("luasnip").s
 local i = require("luasnip").i
 local t = require("luasnip").t
+local f = require("luasnip").f
 local d = require("luasnip").d
 local sn = require("luasnip").sn
 return {
@@ -31,4 +32,5 @@ return {
       return sn(nil, nodes)
     end),
   }),
+  s({ trig = "now" }, { f(function() return os.date "!%Y-%m-%dT%H:%M:%SZ" end, {}) }),
 }
