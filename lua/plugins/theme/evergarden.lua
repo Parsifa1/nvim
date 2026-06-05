bd45cc42c74abb88b8344b839789e6c98e76701f
Parsifa1 <li.aldric@gmail.com>
2026-06-05 17:57:51 +0800

    feat(evergarden): using everforest bg

--- lua/plugins/theme/evergarden.lua
+++ lua/plugins/theme/evergarden.lua
@@ -13,6 +13,7 @@ return {
       DiffAdd = { bg = "#3c4841" },
       DiffChange = { bg = "#384b55" },
       DiffDelete = { bg = "#493b40" },
+      Normal = { fg = "#f8f9e8", bg = "#272e32" },
     },
   },
 }
